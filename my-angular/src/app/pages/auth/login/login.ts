22534377b38f3ab2af3039e3eb61f4cb8931b022
novakvova <novakvova@gmail.com>
2025-08-28 19:22:12 +0300

    Working login is good

--- my-angular/src/app/pages/auth/login/login.ts
+++ my-angular/src/app/pages/auth/login/login.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  //Зберігає логін та пароль користувача
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log("Login user data", this.email, this.password);
+  }
 
 }
