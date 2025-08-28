caca4f6114252b03bf4feadab4848ebe1c53c8de
novakvova <novakvova@gmail.com>
2025-08-28 19:05:41 +0300

    Is good working app

--- my-angular/src/app/app.routes.ts
+++ my-angular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {Home} from './pages/home/home';
+import {Login} from './pages/auth/login/login';
+import {Register} from './pages/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "home", component: Home },
+  { path: "login", component: Login },
+  { path: "register", component: Register },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
